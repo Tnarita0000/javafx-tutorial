64790d4bd4bd43adbffd2ed54fcc6c7bae8be926
naritaku0 <naritatakuya0000@gmail.com>
2016-10-12 00:40:59 +0900

    commit

--- run.sh
+++ run.sh
@@ -1,7 +1,1 @@
-CLASSPATH=.:./lib/mysql-connector-java-5.1.40/mysql-connector-java-5.1.40-bin.jar:./lib/jsch-0.1.54/jsch-0.1.54.jar:$PATH
-export CLASSPATH
-
-
-javac -d . src/controller/*.java
-javac -d . src/app/App.java
 java classes.App
