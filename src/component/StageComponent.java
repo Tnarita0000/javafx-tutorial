82e72934a5fd2610390c13f822b5415c5242d174
naritaku0 <naritatakuya0000@gmail.com>
2016-10-15 18:47:14 +0900

    isolate view logic and stage logic

---
+++ src/component/StageComponent.java
@@ -0,0 +1,24 @@
+package classes;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+
+public class StageComponent {
+  public static Stage stage;
+
+  StageComponent(Stage primaryStage) {
+    this.stage = primaryStage;
+    sendScene("FirstController");
+  }
+
+  public static void sendScene(String controllerName) {
+    if (stage.getScene() == null) {
+      ViewComponent vmanager = new ViewComponent(controllerName);
+      stage.setScene(vmanager.getScene());
+      stage.show();
+    } else {
+      ViewComponent vmanager = new ViewComponent(controllerName);
+      stage.setScene(vmanager.getScene());
+    }
+  }
+}
---
+++ src/component/ViewComponent.java
@@ -0,0 +1,35 @@
+package classes;
+import javafx.fxml.FXMLLoader;
+import java.net.URL;
+import java.io.IOException;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class ViewComponent {
+  public static String controller;
+  public static Scene scene;
+
+  public ViewComponent(String controller) {
+    this.controller = controller;
+    loadFXML();
+  }
+
+  public void loadFXML() {
+    try {
+      Parent root  = FXMLLoader.load(getFXMLLocation());
+      this.scene = new Scene(root);
+    } catch (IOException exception) {
+      throw new RuntimeException(exception);
+    }
+  }
+
+  public URL getFXMLLocation() {
+    String parentDir = "../view/";
+    String fxmlFile  = this.controller + ".fxml";
+    return getClass().getResource(parentDir + fxmlFile);
+  }
+
+  public Scene getScene() {
+    return this.scene;
+  }
+}
