95fc2d8efa9dac99a7f1c23c3c70399ca4e12a22
naritaku0 <naritatakuya0000@gmail.com>
2016-10-16 04:14:03 +0900

    commit

--- src/component/mysql/MySQLSearch.java
+++ src/component/mysql/MySQLSearch.java
@@ -4,33 +4,35 @@ import java.util.Properties;
 import java.sql.*;
 import com.jcraft.jsch.*;
 public class MySQLSearch {
+  public MySQLConnector connector = MySQLManager.connector;
 
-  public static List<String> getDatabases(Connection con) {
-    List<String> databases;
-    databases = new ArrayList<String>();
-    try {
-      PreparedStatement statement = con.prepareStatement("show databases;");
-      ResultSet result = statement.executeQuery();
-      while(result.next()) {
-        String database = result.getString("Database");
-        databases.add(database);
-      }
-    } catch (SQLException e) { e.printStackTrace(); }
-    return databases;
-  }
+  //public static JDBC42ResultSet query(String query) {
+  //  List<String> result = new ArrayList<String>();
+  //  try {
+  //    PreparedStatement statement = connector.prepareStatement(query);
+  //    ResultSet queryResult       = statement.executeQuery();
+  //    while(queryResult.next()) {
+  //      String result = queryResult.getString();
+  //    }
+  //  } catch (SQLException e) { e.printStackTrace(); }
+  //}
 
-  public static List<String> getTables(Connection con, String databaseName) {
-    List<String> tables;
-    tables = new ArrayList<String>();
-    try{
-      PreparedStatement statement = con.prepareStatement("show tables");
-      ResultSet result = statement.executeQuery();
-      while(result.next()) {
-        String table = result.getString("Tables_in_" + databaseName);
-        tables.add(table);
-      }
-    } catch (SQLException e) { e.printStackTrace(); }
-    return tables;
-  }
+  //public static List<String> databases() {
+  //  List<String> databases = new ArrayList<String>();
+  //  while(result.next()) {
+  //    String database = result.getString("Database");
+  //    databases.add(database);
+  //  }
+  //  return databases;
+  //}
+
+  //public static String getTable(String databaseName) {
+  //  List<String> tables = new ArrayList<String>();
+  //  while(result.next()) {
+  //    String table = result.getString("Tables_in_" + databaseName);
+  //    tables.add(table);
+  //  }
+  //  return tables;
+  //}
 
 }
--- src/controller/MySQLDBController.java
+++ src/controller/MySQLDBController.java
@@ -1,21 +1,34 @@
 package sample;
-import javafx.fxml.FXMLLoader;
-import javafx.event.ActionEvent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
-import javafx.scene.control.PasswordField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.Parent;
-import javafx.stage.Stage;
 import java.net.URL;
 import java.io.IOException;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
 
-public class MySQLDBController {
+public class MySQLDBController implements Initializable{
+  @FXML
+  ComboBox databases;
+
   public void pushMe(ActionEvent e) {
     System.out.println("Pushed Me");
   }
+
+  @Override
+  public void initialize(URL url, ResourceBundle rb) {
+    System.out.println(databases);
+    ObservableList<String> options = FXCollections.observableArrayList(
+      "Select Database",
+      "Option 1",
+      "Option 2",
+      "Option 3"
+    );
+    databases.setItems(options);
+  }
+
+
 }
