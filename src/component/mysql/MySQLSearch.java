d879cc1ca155328982af899bdef86a3616065eab
naritaku0 <naritatakuya0000@gmail.com>
2016-10-17 00:18:26 +0900

    commit

--- src/component/mysql/MySQLSearch.java
+++ src/component/mysql/MySQLSearch.java
@@ -42,5 +42,4 @@ public class MySQLSearch {
     } catch (SQLException e) { e.printStackTrace(); }
     return tables;
   }
-
 }
--- src/controller/MySQLDBController.java
+++ src/controller/MySQLDBController.java
@@ -10,6 +10,7 @@ import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableView;
 import javafx.collections.ObservableList;
 import javafx.collections.FXCollections;
 
@@ -18,28 +19,43 @@ public class MySQLDBController implements Initializable{
   ComboBox databases;
   @FXML
   ListView tables;
+  @FXML
+  TableView columns;
 
   public void selectDatabase(ActionEvent e) {
     String dbName = databases.getValue().toString();
     MySQLManager.connector.connectDatabase(dbName);
 
-    /* clear ListView data */
+    /* set table list after cleared tables list in ListView */
     tables.setItems(FXCollections.observableArrayList());
     List<String> queryResult = MySQLSearch.getTables(dbName);
     for(String table : queryResult) {
       tables.getItems().add(table);
     }
-
   }
+
 
   @Override
   public void initialize(URL url, ResourceBundle rb) {
+    /* set databases list to ListView */
     databases.setItems(FXCollections.observableArrayList());
     List<String> queryResult = MySQLSearch.getDatabases();
     for(String database : queryResult) {
       databases.getItems().add(database);
     }
-  }
 
+    /* after clicked table in ListView */
+    tables.setOnMouseClicked((MouseEvent)-> {
+      String tableName = tables.getSelectionModel().getSelectedItem().toString();
+      ResultSet columnsName    = MySQLSearch.query("SHOW COLUMNS FROM "+tableName);
+      ResultSet columnsContens = MySQLSearch.query("SELECT * FROM     "+tableName);
+
+      while(columnsName.next()) {
+        String columnName = columnsName.getString("Field");
+        System.out.println(columnsName);
+      }
+      //columns.getColumns.add
+    });
+  }
 
 }
